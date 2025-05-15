d3c24741fe351d7e0bac77708710ac99f4f198be
Arefashaily <sultana.khairum.arefa@g.bracu.ac.bd>
2025-05-15 20:57:28 +0600

    Update EmailVerificationPromptController.php

--- app/Http/Controllers/auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/auth/EmailVerificationPromptController.php
@@ -1,24 +1,22 @@
-<?php
+
+       <?php
 
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
-class EmailVerificationNotificationController extends Controller
+class EmailVerificationPromptController extends Controller
 {
     /**
-     * Send a new email verification notification.
+     * Display the email verification prompt.
      */
-    public function store(Request $request): RedirectResponse
+    public function __invoke(Request $request): RedirectResponse|View
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false));
-        }
-
-        $request->user()->sendEmailVerificationNotification();
-
-        return back()->with('status', 'verification-link-sent');
+        return $request->user()->hasVerifiedEmail()
+                    ? redirect()->intended(route('dashboard', absolute: false))
+                    : view('auth.verify-email');
     }
 }
